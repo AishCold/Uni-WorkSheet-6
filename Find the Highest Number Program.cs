8dc4f4294bdd2e7da0fc9918dd4c1cc18eb25c84
AishCold <72552823+AishCold@users.noreply.github.com>
2021-02-09 16:17:59 +0000

    Add files via upload

---
+++ Find the Highest Number Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace L6_1_FindHighestValue
+{
+    class Program
+    {
+        //In this program, the highest value in the array is outputted
+        static void Main(string[] args)
+        {
+            uint[] values =
+            {
+                23456, 354345, 2324, 22, 34, 654, 6754, 74557, 345463, 234, 234,
+                234, 43, 25, 6456, 745, 8658, 986, 9678, 2342, 23, 235, 534, 364, 23,
+                234234, 56346, 6435, 3643574, 345, 34523, 563, 2, 674, 78568, 8756, 6587
+            };
+
+            uint highestValue = 0;
+
+            foreach (uint value in values)
+            {
+
+                if(value > highestValue)
+                {
+                    highestValue = value;
+                }
+            }
+
+            Console.WriteLine("The highest value is " + highestValue);
+        }
+    }
+}
